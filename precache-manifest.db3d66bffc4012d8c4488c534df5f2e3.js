c0e6e06015062a5c1ee03de194acd453dcd0a873
{psw9058) <s05150732w@gmail.com>
2022-04-14 14:50:06 +0900

    Updates

---
+++ precache-manifest.db3d66bffc4012d8c4488c534df5f2e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ce06291244d36f6dcd0122519aa0da",
+    "url": "/business-card-maker/index.html"
+  },
+  {
+    "revision": "b4b8bac308fcacd82438",
+    "url": "/business-card-maker/static/css/main.c8ca60a8.chunk.css"
+  },
+  {
+    "revision": "44234a82a52adf1cc78d",
+    "url": "/business-card-maker/static/js/2.5d2e72dd.chunk.js"
+  },
+  {
+    "revision": "7d2282234cd0247bd4712fb2834fc8d9",
+    "url": "/business-card-maker/static/js/2.5d2e72dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b8bac308fcacd82438",
+    "url": "/business-card-maker/static/js/main.edd9ef6f.chunk.js"
+  },
+  {
+    "revision": "ee219eb9a182e22b29d4",
+    "url": "/business-card-maker/static/js/runtime-main.142fd096.js"
+  }
+]);
