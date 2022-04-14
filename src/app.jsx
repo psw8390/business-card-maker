a3fc7c529fa8189d7e6b0c2ed2b37c004f010dca
{psw9058) <s05150732w@gmail.com>
2022-04-14 15:11:02 +0900

    deploy url error

--- src/app.jsx
+++ src/app.jsx
@@ -9,7 +9,7 @@ function App({ FileInput, authService, cardRepository }) {
     <div className={styles.app}>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/business-card-maker">
             <Login authService={authService} />
           </Route>
           <Route path="/maker">
